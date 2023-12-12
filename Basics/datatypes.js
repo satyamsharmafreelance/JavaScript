7205cdf48b0464931d5574dea2904803ce5796e4
Satyam Sharma <148544996+satyamsharmafreelance@users.noreply.github.com>
2023-12-12 09:18:14 +0000

    commit

---
+++ Basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // uses new standards for all the codes present in file 
+// that is it treats all code in newer version of JS
+// alert(2+2); // works in browser not in Nodejs
+
+//  always use new line to write the code
+
+let age = 30;
+let name = "Satyam";
+
+// number ==> 2 * 53 range
+// bigint 
+// string ==> "this is a sring" or 'this is also a string';
+//boolean ==> true/false;
+//Null ==> standalone Object;
+// undefined ==> not defined storages;
+// symbol ==> unique values 
+
+
+// object 
+console.log(typeof undefined);    
+
