5721dc9770797d130df15f02751bf84a652b4037
Satyam Sharma <148544996+satyamsharmafreelance@users.noreply.github.com>
2023-12-13 11:01:09 +0000

    operations done

--- Basics/conversionoperation.js
+++ Basics/conversionoperation.js
@@ -1,3 +1,4 @@
+/*
 let score = 33;
 
 console.log(score);
@@ -5,3 +6,47 @@ console.log(typeof score);
 console.log(String(score));
 console.log(typeof String(score));
 // console.log(typeof Number(score));
+
+*/
+
+// ----------------operations------------------
+let anum = 3;
+
+// console.log(anum);
+
+let negval = -anum;
+// console.log(negval);
+
+// console.log(3+2);
+// console.log(3-2);
+// console.log(3*2);
+// console.log(3/2);
+// console.log(3**2);// raise to term // power term 
+// console.log(67%7);
+
+let stor1 = "Namah";
+let stor2 = " Aste";
+// console.log(stor1 + stor2);
+// console.log("3" + 3);
+// console.log(3 + "3");
+// console.log("3" + "3");
+// console.log("3" + 3 + 2);
+// console.log("3" + 3 + 2);
+// console.log(3 + 3 + "2");
+// console.log((3 + (3 * 4)) / ((2 - 1) ** (33 % 5)));
+
+// console.log(+"2");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+// console.log(num1,num2,num3);
+
+let koinum = 3;
+koinum++
+console.log(koinum);
+// console.log(nayanum);
+
+/*
+Jay maa Ambe
+*/
