8bad4af51b41b0359c02b6c506cb8ce63e796b6b
Satyam Sharma <148544996+satyamsharmafreelance@users.noreply.github.com>
2023-12-19 16:11:09 +0000

    String to be continued

---
+++ Basics/strings.js
@@ -0,0 +1,25 @@
+const employee1 = "Mohan";
+// console.log(employee1);
+
+const employeeid = 123
+// console.log(employeeid);
+
+// console.log(`The First Employee name is ${employee1} his id is ${employeeid}`);
+
+const meraNaam = new String("Satyam-Sharma")
+
+// console.log(meraNaam);
+// console.log(meraNaam[1]);
+
+// console.log(meraNaam.__proto__);
+// console.log(meraNaam.length);
+
+// console.log(meraNaam.toUpperCase());
+// console.log(meraNaam.charAt(9));
+
+// console.log(meraNaam.indexOf('v'));//if search element is not available in storage then indexof() returns -1
+
+const newString = meraNaam.substring(0,14)
+// console.log(meraNaam.length);
+// console.log(newString);
+// console.log(meraNaam.substring(0,6));
