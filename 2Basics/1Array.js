84cace97dd02b5b7e1199c82d0dd0014befec425
Satyam Sharma <148544996+satyamsharmafreelance@users.noreply.github.com>
2023-12-26 11:53:18 +0000

    Array part1 done

---
+++ 2Basics/1Array.js
@@ -0,0 +1,30 @@
+let myarr = [1,2,3,4,5,6]
+let heros = ["Maa","Pita","Bhai","behan"]
+
+let newarr = new Array(1, 2, 3, 4, 5, 6)
+
+console.log(`original : `, myarr); 
+// myarr.push(9)
+// myarr.pop()
+// myarr.unshift(6)
+// myarr.shift()
+// let tempop = myarr.includes(2)
+// let tempop = myarr.indexOf(4)
+
+
+// console.log(newarr);
+// console.log(`Action performed : `,myarr); 
+// console.log(tempop);
+
+let joinarr = myarr.join(); // converts in string
+
+// console.log(joinarr);
+// console.log(typeof joinarr);
+
+console.log(`slice wala o/p `,myarr.slice(1,4));
+
+console.log(`original : `, myarr); 
+
+console.log(`splice wala o/p `,myarr.splice(1,4));
+
+console.log(`original : `, myarr); 
