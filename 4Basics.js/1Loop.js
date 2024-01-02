8c1525df47836954112e5adc0c1eab7153df98af
Satyam Sharma <148544996+satyamsharmafreelance@users.noreply.github.com>
2024-01-02 07:32:35 +0000

    for loop done

---
+++ 4Basics.js/1Loop.js
@@ -0,0 +1,54 @@
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log(`Detected 5 in loop`);
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`this is ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`this is Inner ${j} and in ${i} `);
+//     }
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`this is ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         // console.log(`this is Inner ${j} and in ${i} `);
+//         console.log(i + ' x ' + j + ' = ' + (i*j));
+//     }
+// }
+
+// let myArray = ["satyam", "hitesh", "Raj"]
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+// console.log(myArray.length);
+
+//Break and Continue
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i == 5) {
+//         console.log(`This is ${i} we are break here`);
+//         break
+//     }
+//     console.log(`we are here, ${i}`);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i == 5) {
+//         console.log(`This is ${i}, lets continue further loops`);
+//         continue
+//     }
+//     console.log(`we are here, ${i}`);
+// }
+
