bb15df7f7d0211f5aa136e3320e0b6df4ec3ed5e
Satyam Sharma <148544996+satyamsharmafreelance@users.noreply.github.com>
2024-01-02 17:30:17 +0000

    forIn and forEach Done

---
+++ 4Iterations.js/4forin.js
@@ -0,0 +1,20 @@
+const oneObject = {
+    Hero1 : "Krishna",
+    Hero2 : "Ram",
+    Hero3 : "Hari",
+    Hero4 : "Narsimha"
+}
+
+// for (const key in oneObject) {
+//     // console.log(key);
+//     // console.log(oneObject[key]);
+//     console.log(`key = ${key}, and value = ${oneObject[key]}`);
+// }
+
+const myArray = ["java", "Python", "JavaScript", "Swift", "React"]
+
+// for (const key in myArray) {
+//     // console.log(key);
+//     // console.log(myArray[key]);
+// }
+
---
+++ 4Iterations.js/5foreach.js
@@ -0,0 +1,39 @@
+const myArray = ["java", "Python", "JavaScript", "Satyam", "React"]
+
+// myArray.forEach(function (item) {
+//     console.log(item);
+// });
+
+// myArray.forEach((item) => {
+//     console.log(item);
+// });
+
+// function PrintMe(abc){
+//     console.log(abc);
+// }
+
+// myArray.forEach(PrintMe)
+
+// myArray.forEach((item,ind,arr) => {
+//     console.log(item, ind, arr);
+// })
+
+const myObjectArray = [
+    {
+        languageName : "JavaScript",
+        fileName : "js"
+    },
+    {
+        languageName : "Python",
+        fileName : "py"    
+    },
+    {
+        languageName : "Ruby",
+        fileName : "rb"
+    }
+]
+
+// myObjectArray.forEach((item) => {
+//     console.log(item.languageName + '.' + item.fileName);
+// })
+
