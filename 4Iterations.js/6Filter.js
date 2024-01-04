3385bd039288dda04438e9641ec9a229f8740756
Satyam Sharma <148544996+satyamsharmafreelance@users.noreply.github.com>
2024-01-04 14:59:50 +0000

    filter to be continued

---
+++ 4Iterations.js/6Filter.js
@@ -0,0 +1,19 @@
+// const oneArray = ["Lakshmi", "Saraswati", "savitri", "Radha", "Durga"]
+// const storeFE = oneArray.forEach( (oneArg) => {return oneArg})
+// console.log(storeFE); // Conclusion :- undefined Not Possible
+
+
+// const oneArray =[1,2,3,4,5,6,7,8,9,10]
+// const storeFil = oneArray.filter(item => item > 4)
+// console.log(storeFil);
+
+// const oneArray = [1,2,3,4,5,6,7,8,9,10]
+// const storeFil = oneArray.filter(item => {return item > 4})
+// console.log(storeFil);
+
+// const emptyArray = []
+// emptyArray.forEach( (item) => {
+//     if(item > 10){
+//         emptyArray.push()
+//     }
+// })
